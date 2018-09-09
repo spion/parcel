d80bfedf45d7dc8470ddc469320c14d7bd39d85e
Gorgi Kosev <gorgi.kosev@gmail.com>
2018-09-09 21:21:30 +0100

    add custom getJSON to test

--- test/integration/postcss-plugins/.postcssrc.js
+++ test/integration/postcss-plugins/.postcssrc.js
@@ -2,7 +2,13 @@ module.exports = {
   modules: true,
   plugins: {
     'postcss-modules': {
-      generateScopedName: "_[name]__[local]"
+      generateScopedName: "_[name]__[local]",
+      getJSON: (cssFileName, json) => {
+        let tsFile = Object.keys(json)
+          .map(key => `export let ${key}:string;`)
+          .join('\n');
+        require('fs').writeFileSync(cssFileName + '.d.ts', tsFile);
+      }
     }
   }
 };
